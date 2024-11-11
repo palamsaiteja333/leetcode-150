e5f9f5a74f3b42d080ea411fe008aef932c697e0
Sai Teja <51771508+palamsaiteja333@users.noreply.github.com>
2024-11-11 14:39:31 -0400

    [217] [Time Beats: 71.55%] [Memory Beats: 51.27%] - LeetPush

---
+++ 217-Contains-Duplicate.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums.length ==1){
+            return false;
+        }
+        // 1. Brute Force
+        // for(int i=0; i<nums.length; i++){
+        //     for(int j=i+1; j<nums.length; j++){
+        //         if(nums[i] == nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        // Optimal Solution
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            } 
+            set.add(nums[i]);
+        }
+
+        return false;        
+    }
+}
