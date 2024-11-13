2d3cf7ea2202943ed040baa9f03cff186ba04530
Sai Teja <51771508+palamsaiteja333@users.noreply.github.com>
2024-11-12 21:40:24 -0400

    [49] [Time Beats: 17.94%] [Memory Beats: 7.51%] - LeetPush

--- Arrays & Hashing/49-Group-Anagrams.java
+++ Arrays & Hashing/49-Group-Anagrams.java
@@ -2,16 +2,31 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
         // 1. Sorting
-        Map<String, List<String>> map = new HashMap<>();
+        // TC: m*nlogn
+        // SC: m*n
+        // Map<String, List<String>> map = new HashMap<>();
+        // for(String s : strs){
+        //     char[] charArray = s.toCharArray();
+        //     Arrays.sort(charArray);
+        //     String sortedString = new String(charArray);
+        //     map.putIfAbsent(sortedString, new ArrayList<String>());
+        //     map.get(sortedString).add(s);
+        // }
+        // return new ArrayList<>(map.values());
+
+        // 2. Optimal solution using HashMap
+        Map<String, List<String>> resultList = new HashMap<>();
         for(String s : strs){
-            char[] charArray = s.toCharArray();
-            Arrays.sort(charArray);
-            String sortedString = new String(charArray);
-            map.putIfAbsent(sortedString, new ArrayList<String>());
-            map.get(sortedString).add(s);
+            int[] key = new int[26];
+            for(int i=0; i<s.length(); i++){
+                key[s.charAt(i) - 'a']++;
+            }
+
+            String keyString = Arrays.toString(key);
+            resultList.putIfAbsent(keyString, new ArrayList<String>());
+            resultList.get(keyString).add(s);
         }
 
-        return new ArrayList<>(map.values());
-        
+        return new ArrayList<>(resultList.values());        
     }
 }
