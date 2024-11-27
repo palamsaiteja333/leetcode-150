00b2d668c3a8fcec7333d5bf318141d20ae9b47a
Sai Teja <51771508+palamsaiteja333@users.noreply.github.com>
2024-11-26 22:08:58 -0400

    [74] [Time Beats: 100.00%] [Memory Beats: 54.27%] - LeetPush

--- Binary Search/74-Search-a-2D-Matrix.java
+++ Binary Search/74-Search-a-2D-Matrix.java
@@ -3,30 +3,49 @@ class Solution {
 
         // Brute Force
         // for(int i=0; i<matrix.length; i++){
-        //     for(int j=0; j<matrix[i].length; j++){
-        //         if(target == matrix[i][j]){
-        //             return true;
-        //         }
-        //     }
+        // for(int j=0; j<matrix[i].length; j++){
+        // if(target == matrix[i][j]){
+        // return true;
+        // }
+        // }
         // }
         // return false;
 
         // Little Optimal Solution
-        for(int i=0; i<matrix.length; i++){
-            int low=0;
-            int high = matrix[i].length -1;
+        // for (int i = 0; i < matrix.length; i++) {
+        // int low = 0;
+        // int high = matrix[i].length - 1;
 
-            while(low<=high){
-                int mid = (low + high)/2;
+        // while (low <= high) {
+        // int mid = (low + high) / 2;
+        // if (matrix[i][mid] == target) {
+        // return true;
+        // } else if (matrix[i][mid] > target) {
+        // high = mid - 1;
+        // } else {
+        // low = mid + 1;
+        // }
+        // }
+        // }
+        // return false;
 
-                if(matrix[i][mid] == target){
-                    return true;
-                }else if(matrix[i][mid] > target){
-                    high = mid -1;
-                } else{
-                    low = mid +1;
-                }
+        // Most Optimal Solution
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int low = 0;
+        int high = rows * columns - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midValue = matrix[mid / columns][mid % columns];
+            if (midValue == target) {
+                return true;
+            } else if (midValue > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
+
         }
 
         return false;
